c26fac56908896a7682ba464c94125e48d89680e
Elian Meaca <elianmeaca@gmail.com>
2026-05-22 22:14:02 -0300

    polish

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { ViewportScroller } from '@angular/common';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,17 @@ import { Header } from './header/header';
 })
 export class App {
   protected readonly title = signal('signsmith');
+  constructor(private router: Router, private scroller: ViewportScroller) {
+    this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(() => {
+        const tree = this.router.parseUrl(this.router.url);
+        const fragment = tree.fragment;
+        if (fragment) {
+          setTimeout(() => {
+            document.getElementById(fragment)?.scrollIntoView({ behavior: 'smooth' });
+          }, 100); // wait for component to render
+        }
+      });
+  }
 }

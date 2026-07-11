ebc0bb03a5e152e2be0242360352036dfcb5695c
Elian Meaca <elianmeaca@gmail.com>
2026-07-10 22:28:04 -0300

    finished layout styling

--- src/app/home/contact/whatsapp/whatsapp.ts
+++ src/app/home/contact/whatsapp/whatsapp.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import { simpleWhatsapp } from '@ng-icons/simple-icons';
 
 @Component({
   selector: 'app-whatsapp',
-  imports: [NgIcon],
-  providers: [provideIcons({simpleWhatsapp})],
+  imports: [],
   templateUrl: './whatsapp.html',
   styleUrl: './whatsapp.scss',
 })
--- src/app/home/trabajos/trabajos.ts
+++ src/app/home/trabajos/trabajos.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { LucideMoveRight, LucideSquareArrowOutUpRight } from '@lucide/angular';
 import { SITE_WORKS, Work } from './trabajos-data/trabajos-data';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import { simpleInstagram } from '@ng-icons/simple-icons';
 
 @Component({
   selector: 'app-trabajos',
-  imports: [LucideMoveRight, LucideSquareArrowOutUpRight, NgIcon],
-  providers: [provideIcons({simpleInstagram})],
+  imports: [],
   templateUrl: './trabajos.html',
   styleUrl: './trabajos.scss',
 })
